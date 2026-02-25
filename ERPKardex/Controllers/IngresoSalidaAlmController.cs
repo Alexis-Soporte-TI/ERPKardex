12b2cff53ce8cd95af43bf4fdd0d5148c50b7180
desarrollo-ynnova <desarrollador@corpsaf.com>
2026-02-25 13:09:34 -0500

    estado de producto para ingresosalidaalm

--- ERPKardex/Controllers/IngresoSalidaAlmController.cs
+++ ERPKardex/Controllers/IngresoSalidaAlmController.cs
@@ -519,7 +519,7 @@ namespace ERPKardex.Controllers
                 return Json(new
                 {
                     data = _context.Productos
-                        .Where(p => p.EmpresaId == EmpresaUsuarioId) // <--- CAMBIO AQUÍ
+                        .Where(p => p.EmpresaId == EmpresaUsuarioId && p.Estado == true) // <--- CAMBIO AQUÍ
                         .Select(p => new { p.Id, p.Codigo, p.DescripcionProducto, p.DescripcionComercial, p.CodUnidadMedida })
                         .ToList(),
                     status = true
